570a68634f2d41c0507cb6caceed465f660289c1
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-02-14 21:43:19 +0100

    First

--- app/User.php
+++ app/User.php
@@ -26,4 +26,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+//    /**
+//     * The event map for the model.
+//     *
+//     * @var array
+//     */
+//    protected $events = [
+//        'created' => Registered::class,
+//    ];
 }

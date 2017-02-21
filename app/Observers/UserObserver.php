a71cae8173628fc6de5650fc81b5400ceea677c3
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-02-21 19:40:21 +0100

    Sync

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Observers;
+
+use App\Mail\WelcomeEmailMarkdown;
+use App\User;
+use Mail;
+
+class UserObserver
+{
+    /**
+     * Listen to the User created event.
+     *
+     * @param  User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        dd("prova");
+        //Enviar email
+//        Mail::to($user->email)->send(
+//            new WelcomeEmailMarkdown($user));
+    }
+
+
+}
